1154e5a64aef82badec85764a8b467ff13d08981
lperezcas16 <lperezcas@unbosque.edu.co>
2020-10-30 17:46:14 -0500

    [fix] pep8 style

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,11 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+
     def __str__(self):
         """toString"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """updated the public instance attribute update_at"""
@@ -33,9 +35,3 @@ class BaseModel:
         dicc['updated_at'] = dicc['updated_at'].isoformat()
         dicc['created_at'] = dicc['created_at'].isoformat()
         return dicc
-
-    """Date time"""
-    # moon_time = "2017-06-14T22:31:03.285259"
-    # dtae = datetime.strptime(moon_time, "%Y-%m-%dT%H:%M:%S.%f")
-    # print(dtae)
-    # print(type(dtae))
