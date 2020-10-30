49de62fcfda76db6464168d488b40f728bef17c8
thiago1623 <thiago.96t16@gmail.com>
2020-10-30 13:57:42 -0500

    [update]--FILES--(new changes)

--- models/base_model.py
+++ models/base_model.py
@@ -7,13 +7,17 @@ from datetime import datetime
 class BaseModel:
     """ Generate random ID"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Contructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = datetime.now()
-
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "update_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
     def __str__(self):
         """toString"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
