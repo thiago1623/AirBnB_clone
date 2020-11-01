8c1790c6e14c1edc8399c7618f0dc60bc62c50de
thiago1623 <thiago.96t16@gmail.com>
2020-11-01 17:38:32 -0500

    [update]--CONSOLE--(add new changes)

--- console.py
+++ console.py
@@ -2,15 +2,24 @@
 """
 the console
 """
-import sys
 import cmd
+import models
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+found_class = {"Amenity": Amenity, "BaseModel": BaseModel, "Place": Place,
+               "Review": Review, "State": State, "User": User}
 
 
 class HBNBCommand(cmd.Cmd):
     """Implementing cmd module that quits out of the interpreter when
     the user types quit or EOF.
     """
-    intro = "---Welcome to hbnb! Type (?) or (help) to list commands.---"
+    intro = "---Welcome to hbnb console! Type (?) or (help)to list commands---"
     prompt = "(hbnb)"
 
     #################
@@ -30,18 +39,37 @@ class HBNBCommand(cmd.Cmd):
         """when line is empty do nothing"""
         return False
 
+    def do_create(self, inp):
+        """command to create an new instance"""
+        try:
+            arguments = inp.split()
+            if len(arguments) == 0:
+                print("** class name missing **")
+                return False
+            if arguments[0] in found_class:
+                object = found_class[arguments[0]]()
+            else:
+                print("** class doesn't exist **")
+                return False
+            print(object.id)
+            object.save()
+        except Exception:
+            raise
 
     #################
-   # Help Functions#
-   #################
+    # Help Functions#
+    #################
 
     def help_quit(self):
-       """shows command help"""
-       print("Quit command to exit the program")
+        """shows command help"""
+        print("Quit command to exit the program")
 
     def help_EOF(self):
         """ shows command help"""
         print("CTRL + D (EOF) to exit the program")
+
+    def help_create(self):
+        print("Usage: create <valid class name>")
 
 
 if __name__ == '__main__':
