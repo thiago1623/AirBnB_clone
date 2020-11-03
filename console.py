608504da504567be9360e61d72b48409e7d14252
thiago1623 <thiago.96t16@gmail.com>
2020-11-02 20:03:03 -0500

    [update]--CONSOLE--(add new changes)

--- console.py
+++ console.py
@@ -165,6 +165,19 @@ class HBNBCommand(cmd.Cmd):
     def help_create(self):
         print("Usage: create <valid class name>")
 
+    def help_show(self):
+        print("Usage: show <valid class name> <valid id>")
+
+    def help_destroy(self):
+        print("Usage: destroy <valid class name> <valid id>")
+
+    def help_all(self):
+        print("Usage: all OR all <valid class name>")
+
+    def help_update(self):
+        print("Usage: update <valid class name>", end="")
+        print("<valid id> <attribute name> <attribute value>")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
