11c14a7c838c9e08060538e3ed243184fc9f53ea
thiago1623 <thiago.96t16@gmail.com>
2020-11-03 22:17:16 -0500

    [update]--Models--(add new changes)

--- models/review.py
+++ models/review.py
@@ -8,3 +8,6 @@ class Review(BaseModel):
     place_id = ""
     user_id = ""
     text = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -6,3 +6,6 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """State class"""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super(State, self).__init__(*args, **kwargs)
