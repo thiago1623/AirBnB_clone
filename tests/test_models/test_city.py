921debe8147a7717310ffb5ba6bc2f4b1d692155
thiago1623 <thiago.96t16@gmail.com>
2020-11-03 19:02:02 -0500

    [update]--TESTS--(add new changes)

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import sys
+import unittest
+import pep8
+from models.city import City
+
+
+class Test_CityModel(unittest.TestCase):
+    """
+    Test the amenity model class
+    """
+    def testpep8(self):
+        """ testing codestyle """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        user_path = 'models/city.py'
+        result = pepstylecode.check_files([user_path])
+
+    def setUp(self):
+        self.model = City()
+        self.model.save()
+
+    def test_Amenity_initialization(self):
+        self.assertTrue(hasattr(self.model, "state_id"))
+        self.assertTrue(hasattr(self.model, "name"))
+        self.assertEqual(self.model.state_id, "")
+        self.assertEqual(self.model.name, "")
+
+
+if __name__ == '__main__':
+    sys.path.append("../..")
+    unittest.main()
